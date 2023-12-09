9039c533da3d717cf432973564fd8be1abdc1c45
Pat Needham <patrick.h.needham@gmail.com>
2023-12-09 12:20:08 -0500

    using shadcn to generate radix ui components for reusability

---
+++ app/components/ui/index.ts
@@ -0,0 +1,5 @@
+// https://ui.shadcn.com/docs/installation/next
+// all components in this directory are scaffolded
+// using https://ui.shadcn.com/docs
+
+export { Button, buttonVariants, type ButtonProps } from './button';
