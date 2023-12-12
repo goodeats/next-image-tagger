e1b283978b0f9f0fde22900ba524492e6bcf04ed
Pat Needham <patrick.h.needham@gmail.com>
2023-12-12 00:10:21 -0500

    cleaned up image tagger left column

--- app/components/images/image-tagger.tsx
+++ app/components/images/image-tagger.tsx
@@ -7,33 +7,38 @@ import ImageTagsForm from './tags-form';
 import { Badge } from '../ui';
 
 export default function ImageTagger({ image }: { image: IImage }) {
-  const { url, alt, tags, collection } = image;
+  const { url, alt, tags } = image;
   const imageTagIds = tags.map((tag) => tag.id);
 
+  const ColumnImage = () => (
+    <Image
+      loader={customLoader}
+      src={url}
+      alt={alt || 'no alt'}
+      width={500}
+      height={500}
+      objectFit="contain"
+      className="rounded-md mb-4"
+    />
+  );
+
   const Tags = () => (
-    <div>
-      <p>Tags:</p>
-      {tags?.map((tag) => (
-        <Badge key={tag.id} className="mr-2 mb-2">
-          {tag.name}
-        </Badge>
-      ))}
+    <div className="flex flex-col space-y-4">
+      <h6 className="text-h6 mb-2">Current Tags</h6>
+      <div>
+        {tags?.map((tag) => (
+          <Badge key={tag.id} className="mr-2 mb-2">
+            {tag.name}
+          </Badge>
+        ))}
+      </div>
     </div>
   );
 
   return (
     <div className="grid gap-4 md:grid-cols-2">
       <div>
-        <Image
-          loader={customLoader}
-          src={url}
-          alt={alt || 'no alt'}
-          width={500}
-          height={500}
-          objectFit="contain"
-          className="rounded-md mb-4"
-        />
-        <div>Collection: {collection?.name || 'n/a'}</div>
+        <ColumnImage />
         <Tags />
       </div>
       <ImageTagsForm image={image} tagIds={imageTagIds} />
