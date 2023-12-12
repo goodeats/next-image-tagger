fcaddc296b05c1189ca6885814ca0aabcbb496e4
Pat Needham <patrick.h.needham@gmail.com>
2023-12-12 01:21:16 -0500

    remove display card

--- app/components/shared/index.ts
+++ app/components/shared/index.ts
@@ -1,6 +1,5 @@
 // assembling components from app/components/ui/index.ts
 
-export { DisplayCard } from './display-card';
 export { DisplayTable } from './display-table';
 export { Errors, ErrorList, type ListOfErrors } from './errors';
 export { Field, FieldContainer } from './field';
